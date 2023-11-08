e8a3a51d9db3ac14884f3f75d39dc463a0a32d2f
Anthony E <AnthonyDEpps@gmail.com>
2023-11-08 14:07:39 -0500

    Bug fix: I am code blind

--- person.py
+++ person.py
@@ -1,61 +1,63 @@
+MAX_PERSON_NAME_LENGTH: int = 40
+PHONE_NUMBER_LENGTH: int = 10
+MAX_ADDRESS_LENGTH: int = 100
+MAX_EMAIL_LENGTH: int = 100
+
+
 class Person(object):
 
-    # Deafault Constructor
-    #Person has:
+    # Default Constructor
+    # Person has:
     # Name, Address, Phone Number, and email
-    def __init__(self, name, address, phone_number, email):
-        self.name = name;
-        self.address = address;
-        self.phone_number = phone_number;
-        self.email = email;
+    def __init__(self, name="", address="", phone_number="", email=""):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.email = email
 
-    #Getters using decorator
-    @property
+    def __str__(self):
+        return "Name:" + self.name + ", Address:" + self.address + ", Phone: " + self.phone_number + ", Email: " + self.email
+
+    # Getters using decorator
+
     def name(self):
         return self.name
-    @property
+
     def address(self):
         return self.address
-    @property
-    def phoneNum(self):
+
+    def phone_number(self):
         return self.phone_number
-    @property
+
     def email(self):
         return self.email
-    
-    #Setters using decorator
-    @name.setter
+
+    # Setters using decorator
     def name(self, value):
         self.name = value
 
-    @address.setter
     def address(self, value):
-        if (len(value) < 13): # make sure lenght is enough
-            print(f"{value} is not a valid phone number")
-        # add format check
-        # add digit check
-        else:
-            self.address = value
+        self.address = value
 
-    @phoneNum.setter
-    def phoneNum(self, value):
+    def phone_number(self, value):
         self.phone_number = value
 
-    @email.setter
     def email(self, value):
         self.email = value
 
- #Main function for testing (well the test before running the Test script)   
+
+# Main function for testing (well the test before running the Test script)
 def main():
-    #Basic test - ensure basic sets work, pre adding conditions (phone# needs x amount of nums)
-    ant = Person
+    # Basic test - ensure basic sets work, pre adding conditions (phone# needs x amount of nums)
+    ant = Person()
     ant.name = "Anthony"
     print(ant.name)
     ant.address = "123 I Live Here Lane"
     print(ant.address)
-    ant.phone_number = "123-456-8910"
+    ant.phone_number = "1234567890"
     print(ant.phone_number)
 
+    print(ant)
 
 
 if __name__ == "__main__":
